ea6950d372db0720a30d1d739eb95956af98cc80
Rudy Crespo <crespor@montclair.edu>
2019-10-29 12:20:05 -0400

    adding components, adding possible hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import drugsData from './drugs.json'
+
 const App: React.FC = () => {
+  const [searchResults, setSearchResults] = useState([]);
+  const [selectedSong, setSelectedSong] = useState(null);
+
   return (
     <div className="App">
     </div>
