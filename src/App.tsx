eea6e366b091ecaa9e94c8797d20e49113a3aa82
Rudy Crespo <crespor@montclair.edu>
2019-10-29 16:09:48 -0400

    successful genius api get request

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from "react";
+import axios from "axios";
+import { debounce } from "lodash";
 
-import drugsData from './drugs.json'
+import drugsData from "./drugs.json";
 
-import Search from './components/Search';
+import Search from "./components/Search";
 
 const App = () => {
   const [searchResults, setSearchResults] = useState([]);
   const [selectedSong, setSelectedSong] = useState(null);
 
-  const fetchSearchResults = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(e.currentTarget.value);
-  }
+  const fetchSearchResults = (inputVal: any) => {
+    axios
+      .get("https://api.genius.com/search", {
+        params: {
+          q: inputVal,
+          access_token: process.env.REACT_APP_GENIUS_API_TOKEN
+        }
+      })
+      .then(response => {
+        const { data: {
+          response: {
+            hits
+          }
+        } } = response
+
+        setSearchResults(hits);
+      });
+  };
+
+  const debouncedSearchResults = useCallback(
+    debounce(fetchSearchResults, 500),
+    []
+  );
 
   return (
     <div className="App">
-      <Search textChange={fetchSearchResults} />
+      <Search
+        textChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          debouncedSearchResults(e.currentTarget.value)
+        }
+      />
     </div>
   );
-}
+};
 
 export default App;
