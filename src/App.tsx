7128bfd3911dce2ca6d21255ee20ef7876ff2188
Rudy Crespo <crespor@montclair.edu>
2019-12-06 11:12:50 -0500

    scan lyrics functionality removed after being moved to api

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React, { useState, useCallback } from "react";
-import { debounce, escapeRegExp } from "lodash";
+import { debounce } from "lodash";
 import styled, { ThemeProvider } from "styled-components";
 import { modularScale } from "polished";
-import nlp from "compromise";
 
 import GlobalStyles from "./theme/globalStyles";
 import variables from "./theme/variables";
-
-import drugsData from "./data/drugs.json";
 
 import SVGIconography from "./components/SVGIconography";
 import Header from "./components/Header";
@@ -18,7 +15,6 @@ import DrugMentions from "./components/DrugMentions";
 import LoadingLyrics from "./components/LoadingLyrics";
 import LoadingDrugMentions from "./components/LoadingDrugMentions";
 
-import DrugReference from "./types_interfaces/DrugReference";
 import DrugReferences from "./types_interfaces/DrugReferences";
 
 const baseApiURL = "https://drug-mentions-api.herokuapp.com";
@@ -26,6 +22,7 @@ const baseApiURL = "https://drug-mentions-api.herokuapp.com";
 type SelectedSong = {
   title: string;
   lyrics: string;
+  drugReferences: DrugReferences;
 };
 
 const SiteWrapper = styled.div`
@@ -47,15 +44,9 @@ const SplitPane = styled.div`
 const App = () => {
   const [searchResults, setSearchResults] = useState<any[] | null>(null);
   const [selectedSong, setSelectedSong] = useState<SelectedSong | null>(null);
-  const [drugReferences, setDrugReferences] = useState<DrugReferences | null>(
-    null
-  );
-  const [highlightedLyrics, setHighlightedLyrics] = useState<string | null>(
-    null
-  );
   const [isResultsOpen, setResultsStatus] = useState(false);
   const [isSearchLoading, setSearchLoadingState] = useState(false);
-  const [isLyricsLoading, setLyricsLoadingState] = useState(false);
+  const [isSongLoading, setSongLoadingState] = useState(false);
 
   const fetchSearchResults = async (inputVal: any) => {
     setSearchLoadingState(true);
@@ -86,156 +77,17 @@ const App = () => {
     setResultsStatus(false);
   };
 
-  const drugRegex = (drugName: string) => {
-    const commonCharacters = ".,/#!$%^&*;:{}=\\-_`~@é";
-    const lookBehindCharacterSet = `[${commonCharacters}]`;
-    const lookAheadCharacterSet = `[${commonCharacters}'‘’“”"]`;
-    // console.log(`(?<!${lookAheadCharacterSet})\\b${escapeRegExp(
-    //   drugName
-    // )}s?(?!${lookBehindCharacterSet}\\b)\\b(?![.*])`)
-
-    return `(?<!${lookBehindCharacterSet})\\b${escapeRegExp(
-      drugName
-    )}s?(?!${lookAheadCharacterSet}\\b)\\b(?![.*])`;
-  };
-
-  const highlightLyrics = (drugNames: string[], lyrics: string): string => {
-    let drugNamesRegexes: string[];
-    let highlightRegex: RegExp;
-    let highlightedLyrics: string = lyrics;
-
-    type PartOfSpeechType = {
-      abbr: string;
-      title: string;
-    }
-
-    const partOfSpeech = (word:string): PartOfSpeechType => {
-      class PartOfSpeechAbbr implements PartOfSpeechType {
-        abbr: string;
-        title: string;
-        constructor(abbr:string, title:string) {
-          this.abbr = abbr;
-          this.title = title;
-        }
-      }
-
-      if (nlp(word).match("#Adjective").found) {
-        return new PartOfSpeechAbbr('adj.', 'Adjective');
-      } else if (nlp(word).match("#Pronoun").found) {
-        return new PartOfSpeechAbbr("pron.", "Pronoun");
-      } else if (nlp(word).match("#Verb").found) {
-        return new PartOfSpeechAbbr("v.", "Verb");
-      } else {
-        return new PartOfSpeechAbbr('', '');
-      }
-    }
-
-    drugNamesRegexes = Array.from(new Set(drugNames)).map(drug =>
-      drugRegex(drug)
-    );
-
-    highlightRegex = new RegExp(`${drugNamesRegexes.join("|")}`, "igm");
-
-    if (drugNames.length) {
-      highlightedLyrics = lyrics.replace(
-        highlightRegex,
-        word => {
-          return `<mark class="highlighted">${word}${
-            partOfSpeech(word).abbr && partOfSpeech(word).title
-              ? ` (<abbr title="${partOfSpeech(word).title}">${
-                  partOfSpeech(word).abbr
-                }</abbr>)`
-              : ""
-          }</mark>`;
-        }
-      );
-    }
-
-    return highlightedLyrics;
-  };
-
-  const scanLyricsForDrugs = (drugs: any[], lyrics: string) => {
-    const drugReferences: DrugReference[] = [];
-    const replacedStr = "\n[replaced]\n"; // small hack until compromise library fixes bug that removes whitespace when words are deleted/replaced
-    const lyricsHeadersRegex = /(?:(\[|\()(Intro|Verse|Chorus|Bridge)).*(?:\]|\))/gim;
-    const sanitizedLyrics = nlp(lyrics.replace(lyricsHeadersRegex, " "))
-      .replace("#Contraction", replacedStr)
-      .out("text");
-    const drugRefMatches = (
-      drugName: string,
-      lyrics: string
-    ): RegExpMatchArray | null => {
-      const regex: RegExp = new RegExp(drugRegex(drugName), "igm");
-      const matches = lyrics.match(regex);
-
-      return matches;
-    };
-    const drugInRefArray = (drugName: string): DrugReference | undefined =>
-      drugReferences.find(
-        (drugMentioned: DrugReference) => drugMentioned.drugName === drugName
-      );
-    let drugNames: string[];
-    let totalDrugReferences: number;
-
-    drugs.forEach(drug => {
-      const drugTypesMentioned = drugRefMatches(drug.drugType, sanitizedLyrics);
-
-      if (drugTypesMentioned) {
-        drugReferences.push({
-          drugName: drug.drugType,
-          referenceCount: drugTypesMentioned.length,
-          isStreetName: false
-        });
-      }
-
-      drug.streetNames.forEach((streetName: string) => {
-        const streetNamesMentioned = drugRefMatches(
-          streetName,
-          sanitizedLyrics
-        );
-
-        if (streetNamesMentioned) {
-          if (drugInRefArray(streetName)) {
-            let { drugTypes } = drugInRefArray(streetName)!;
-            if (!drugTypes!.includes(drug.drugType))
-              drugTypes!.push(drug.drugType);
-          } else {
-            drugReferences.push({
-              drugName: streetName,
-              referenceCount: streetNamesMentioned.length,
-              isStreetName: true,
-              drugTypes: [drug.drugType]
-            });
-          }
-        }
-      });
-    });
-
-    drugNames = drugReferences.map(drugReference => drugReference.drugName);
-    totalDrugReferences = drugReferences.reduce(
-      (acc, reference) => acc + reference.referenceCount,
-      0
-    );
-
-    setHighlightedLyrics(highlightLyrics(drugNames, lyrics.trim()));
-    setDrugReferences({
-      totalReferences: totalDrugReferences,
-      references: drugReferences
-    });
-  };
-
   const fetchSong = async (songId: string | undefined) => {
-    setLyricsLoadingState(true);
+    setSongLoadingState(true);
 
     try {
       const response = await fetch(`${baseApiURL}/song-lyrics/${songId}`);
       const song = await response.json();
 
       setSelectedSong(song);
-      scanLyricsForDrugs(drugsData.drugs, song.lyrics);
-      setLyricsLoadingState(false);
+      setSongLoadingState(false);
     } catch (error) {
-      setLyricsLoadingState(false);
+      setSongLoadingState(false);
       throw new Error(error);
     }
   };
@@ -267,24 +119,22 @@ const App = () => {
             isResultsOpen={isResultsOpen}
             isLoading={isSearchLoading}
           />
-          {isLyricsLoading ? (
+          {isSongLoading ? (
             <SplitPane>
               <LoadingDrugMentions />
               <LoadingLyrics />
             </SplitPane>
           ) : (
-            selectedSong &&
-            highlightedLyrics &&
-            drugReferences && (
-              <SplitPane aria-busy={isLyricsLoading}>
+            selectedSong && (
+              <SplitPane aria-busy={isSongLoading}>
                 <DrugMentions
-                  totalReferences={drugReferences.totalReferences}
-                  references={drugReferences.references}
+                  totalReferences={selectedSong.drugReferences.totalReferences}
+                  references={selectedSong.drugReferences.references}
                 />
 
                 <Lyrics
                   songDetails={selectedSong.title}
-                  lyrics={highlightedLyrics}
+                  lyrics={selectedSong.lyrics}
                 />
               </SplitPane>
             )
